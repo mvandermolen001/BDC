0e921ed2a134f6c7be73e4c7ce63a170ba13bf2e
mvandermolen001 <92088207+mvandermolen001@users.noreply.github.com>
2024-06-12 13:08:24 +0200

    Update merge_chunks.py

--- Assignment3/merge_chunks.py
+++ Assignment3/merge_chunks.py
@@ -1,21 +1,34 @@
+"""
+A script containing the tools necessary to process merge chunks of a fastq file.
+The script merges the results into a dictionary with the key as the column position and
+the value as a list containing the average and the length.
+
+As output this script writes the dictionary to the standard output in the format:
+key, average score
+"""
 import sys
 import ast
 
 
 def write_results(results):
+    """
+    Write results to the standard output using the merged results
+    :param results: These are the merged results
+    """
     for key, value in results.items():
-        print(f"{key},{sum(value) / len(value)}")
+        print(f"{key},{value[0] / value[1]}")
 
 
 if __name__ == "__main__":
-    # ast.literal.eval to go from string to literal
     merged_results = {}
-    for i in sys.stdin:
-        i = ast.literal_eval(i)
-        for j in range(len(i)):
-            if j not in merged_results.keys():
-                merged_results[j] = i[j]
+    for data in sys.stdin:
+        # ast.literal.eval to go from string to literal
+        data = ast.literal_eval(data)
+        for count, sublist in enumerate(data):
+            if count not in merged_results:
+                merged_results[count] = [sublist[0], sublist[1]]
             else:
-                for score in i[j]:
-                    merged_results[j].append(score)
+                merged_results[count][0] += sublist[0]
+                merged_results[count][1] += sublist[1]
     write_results(merged_results)
+    
