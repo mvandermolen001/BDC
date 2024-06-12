417fc3c7dd6679b7e7cfb4627f22cf0c9968e016
mvandermolen001 <92088207+mvandermolen001@users.noreply.github.com>
2024-06-12 13:07:29 +0200

    Update assignment3.sh so that multiple files can be processed

--- Assignment3/assignment3.sh
+++ Assignment3/assignment3.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-echo "$1"
-# TO DO: See if you can make this for multiple files
-cat "$1" | parallel --pipe -N8 "python3 assignment3.py"|  python3 merge_chunks.py
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+ASSIGNMENT_PATH=$SCRIPT_DIR"/assignment3.py"
+
+for var in "$@"
+do
+    echo "$var"
+    cat "$var" | parallel --pipe -N1638400 --jobs 4 --slf nodes.txt -k python3 "${ASSIGNMENT_PATH}" |  python3 merge_chunks.py
+done
