48fad3454dec86f0b5611e0cbf391659d8e6c2d9
mvandermolen001 <92088207+mvandermolen001@users.noreply.github.com>
2024-06-13 09:00:42 +0200

    Update assignment3.sh piped to a .csv as output

--- Assignment3/assignment3.sh
+++ Assignment3/assignment3.sh
@@ -6,5 +6,5 @@ ASSIGNMENT_PATH=$SCRIPT_DIR"/assignment3.py"
 for var in "$@"
 do
     echo "$var"
-    cat "$var" | parallel --pipe -N1638400 --jobs 4 --slf nodes.txt -k python3 "${ASSIGNMENT_PATH}" |  python3 merge_chunks.py
+    cat "$var" | parallel --pipe -N1638400 --jobs 4 --slf nodes.txt -k python3 "${ASSIGNMENT_PATH}" |  python3 merge_chunks.py > output.csv
 done
