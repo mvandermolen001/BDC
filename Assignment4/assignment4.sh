e06480a51e777682823c59daba78e48bd4d97111
mvandermolen001 <92088207+mvandermolen001@users.noreply.github.com>
2024-06-26 08:56:38 +0200

    fixed the double '$' in line 15

--- Assignment4/assignment4.sh
+++ Assignment4/assignment4.sh
@@ -12,5 +12,5 @@ ASSIGNMENT_PATH=$SCRIPT_DIR"/assignment4.py"
 
 for var in "$@"
 do
-  mpirun -n 4 python3 "${ASSIGNMENT_PATH}" $"$var" -o ./output.csv
+  mpirun -n 4 python3 "${ASSIGNMENT_PATH}" "$var" -o ./output.csv
 done
