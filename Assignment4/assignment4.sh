4f252447f441aff1ecc736ea1685bc32cdcec786
mvandermolen001 <92088207+mvandermolen001@users.noreply.github.com>
2024-06-26 08:47:17 +0200

    Added slurm capability to assignment4.sh

--- Assignment4/assignment4.sh
+++ Assignment4/assignment4.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 #SBATCH --job-name=assignment4
 #SBATCH --nodes=5
-#SBATCH --ntasks=3
-#SBATCH --time=00:10:00
-#SBATCH --cpus-per-task=4
+#SBATCH --ntasks=5
+#SBATCH --time=00:08:00
+#SBATCH --cpus-per-task=1
+#SBATCH --partition=assemblix
+#SBATCH --nodelist=assemblix2019
 
-srun python3 assignment4.py [ENTER FILE HERE] -o ./output.csv
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+ASSIGNMENT_PATH=$SCRIPT_DIR"/assignment4.py"
+
+for var in "$@"
+do
+  mpirun -n 4 python3 "${ASSIGNMENT_PATH}" $"$var" -o ./output.csv
+done
