9210dba69af1508febaa9c36c72736ba8e06cd5d
mvandermolen001 <92088207+mvandermolen001@users.noreply.github.com>
2024-07-02 14:11:13 +0200

    removed typos

--- Assignment5/assignment5.py
+++ Assignment5/assignment5.py
@@ -37,7 +37,7 @@ def extract_features(gbff_file):
                 lower, upper, length = get_length(str(feature.location))
                 if feature.type == "gene":
                     rows.append(
-                        Row(type=1, type_inf=feature.type,
+                        Row(type=2, type_inf=feature.type,
                             start=lower, end=upper, length=length, organism_name=organism,
                             record_name=gb_record.name))
                 elif feature.type == "CDS":
@@ -58,26 +58,29 @@ def main():
     Main function that creates the dataframe and gets the answers to the assignments questions
     """
     # Location of archea file
-    archea_file = "/data/datasets/NCBI/refseq/ftp.ncbi.nlm.nih.gov/refseq/release/archaea/archaea.3.genomic.gbff"
+    archaea_file = "/data/datasets/NCBI/refseq/ftp.ncbi.nlm.nih.gov/refseq/release/archaea/archaea.3.genomic.gbff"
+    test_file = "/homes/mvandermolen/thema_12/BDC/Assignment5/test_files/test_2.gbff"
     # Get the rows of each features
-    archea_rows = extract_features(archea_file)
+    archaea_rows = extract_features(test_file)
     spark = SparkSession.builder.getOrCreate()
-    archae_dataframe = spark.createDataFrame(archea_rows)
+    archaea_dataframe = spark.createDataFrame(archaea_rows)
 
-    # Average amount of features per archae
-    archae_dataframe.groupby("organism_name", "record_name").count().groupby("organism_name").avg("count").show()
-    # Minimum and maximum length of proteins per archae
-    archae_dataframe.filter("type == 1").groupby("organism_name").agg({'length': 'min'}).show()
-    archae_dataframe.filter("type == 1").groupby("organism_name").agg({'length': 'max'}).show()
+    # TO DO: REMOVE GENES WHICH HAVE A CDS, KEEP GENES THAT DON'T AND ASSIGN NON-CODING
+
+    # Average amount of features per archaea
+    archaea_dataframe.groupby("organism_name", "record_name").count().groupby("organism_name").avg("count").show()
+    # Minimum and maximum length of proteins per archaea
+    archaea_dataframe.filter("type == 1").groupby("organism_name").agg({'length': 'min'}).show()
+    archaea_dataframe.filter("type == 1").groupby("organism_name").agg({'length': 'max'}).show()
     # Average length of a feature
-    archae_dataframe.agg({'length': 'mean'}).show()
+    archaea_dataframe.agg({'length': 'mean'}).show()
     # Ratio between coding and non coding features
-    coding_count = archae_dataframe.filter("type == 1").count()
+    coding_count = archaea_dataframe.filter("type == 1").count()
     # Add one to prevent division by zero
-    non_coding_count = archae_dataframe.filter("type == 0").count() + 1
+    non_coding_count = archaea_dataframe.filter("type == 0").count() + 1
     print(f"This is the ratio of coding to non coding features in this file {coding_count / non_coding_count}")
     # Remove all non-coding features and save this into a seperate dataframe
-    coding_df = archae_dataframe.filter("type == 1")
+    coding_df = archaea_dataframe.filter("type == 1")
     coding_df.write.save("coding_dataframe")
 
 
